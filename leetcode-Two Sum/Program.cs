012ed54bd64c041ecec0a6c294e351fd3ba5a744
Vovaldy <raek06@mail.ru>
2023-10-09 21:53:25 +0300

    Добавьте файлы проекта.

---
+++ leetcode-Two Sum/Program.cs
@@ -0,0 +1,9 @@
+﻿using leetcode_Two_Sum;
+
+Console.WriteLine("Two Sum\n");
+Console.WriteLine("Введите а б\n");
+int[]? nums = { 1, 2, 3 };
+int target = 5;
+var twoSum = new Two_sum();
+int[]? answer = twoSum.TwoSum(nums, target);
+Console.WriteLine(answer);
---
+++ leetcode-Two Sum/Two sum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_Two_Sum
+{
+    internal class Two_sum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[]? answer = new int[2];
+            for(int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if ((nums[i] + nums[j]) == target) { answer[0] = i; answer[1] = j; }
+                }
+            }
+            return answer;
+        }
+    }
+}
